95b5b4ffafd37aada997c9bbc65836edb84d6827
Tara Dehdari <tara.dehdari@gmail.com>
2023-06-05 20:16:59 -0700

    added ball

--- main.py
+++ main.py
@@ -35,6 +35,13 @@ paddle_b.penup()
 paddle_b.goto(350,0)
 
 # Ball
+ball = turtle.Turtle()
+ball.speed(0)
+ball.shape("square")
+ball.color("white")
+ball.penup()
+ball.goto(0,0)
+
 
 #main game loop
 while True:
